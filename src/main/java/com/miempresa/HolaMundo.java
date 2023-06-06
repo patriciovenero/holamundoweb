84a2ee88f940c5be5a55ba7f2a5d630de3e8567a
Patricio Venero Medina <patricio.venero@tecsup.edu.pe>
2023-06-06 10:51:05 -0500

    holamundo

---
+++ src/main/java/com/miempresa/HolaMundo.java
@@ -0,0 +1,40 @@
+package com.miempresa;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HolaMundo {
+	@RequestMapping("/saludar")
+	public String Saludar() {
+		String saludo = "<h1>Hola mundo desde <b>Spring Boot</b> </h1>";
+		 saludo += "<br><a href='/'>Volver</a>";
+				return saludo;
+	}
+	@RequestMapping("/misdatos")
+	public String MisDatos() {
+		String nombres = "Patricio Venero Medina";
+		int edad = 18;
+		String curso = "diseño y desarrollo de software";
+		String foto = "perrito.jpg";
+		String datos = "<table border=1>";
+		datos += "<tr><td colspan=2><img src="+ foto +"> <br> <center><h1>¿por que java?</h1></center> </td></tr>";
+		datos += "<tr><td><h3>Me llamo</h3></td><td>"+ nombres +" </td></tr>";
+		datos += "<tr><td><h3>mi edad es</h3></td><td>"+ edad +" </td></tr>";
+		datos += "<tr><td><h3>Estoy en el curso de </h3></td><td>"+ curso +" </td></tr>";
+		datos += "</table>";
+		 datos += "<br><a href='/'>Volver</a>";
+		
+		return datos;
+		
+	}
+
+	  @RequestMapping("/")
+		public String Indice() {
+			String enlaces = "<a href='/saludar' >Mostrar Saludo</a> <br>";
+			enlaces += "<a href='/misdatos' > Mostrar Mis Datos</a><br>";
+			enlaces += "<a href='index.html' > Mostrar Mi index</a>";
+			return enlaces;
+	  }
+}
